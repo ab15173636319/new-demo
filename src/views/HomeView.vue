30c95ff36151a2d4f369db774c00bceee61bb36a
Luizhen <3435350677@qq.com>
2022-10-17 10:53:31 +0800

    1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="tc pd10">
       <i class="iconfont">&#xe613;</i>
-      <div>new demo</div>
+      <div>new demo1</div>
       <div>{{ title }}</div>
     </div>
     <div class="menus">
